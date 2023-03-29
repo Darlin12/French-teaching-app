d0d91a19aa5a8e6804480340abbeab89445f7d29
darli <darlinmanuelc05@gmail.com>
2023-03-29 12:52:49 -0400

    Primer Commit

---
+++ app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.frenchteacherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_main);
+
+
+      }
+
+      public void sayTheColor(View view){
+           Button clickedBtn = (Button) view;
+
+          MediaPlayer mp = MediaPlayer.create(this, getResources().getIdentifier(clickedBtn.getTag().toString(), "raw",
+                  getPackageName()
+          ));
+          mp.start();
+      }
+
+
+
+
+}
+
